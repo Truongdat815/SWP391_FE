c7c961195dda0c0db31712738ae96ee8be2c78f0
unknown <truonglongkt2021@gmail.com>
2025-10-02 19:49:14 +0700

    hello

--- src/layouts/Adminlayout.jsx
+++ src/layouts/Adminlayout.jsx
@@ -5,7 +5,7 @@ const AdminLayout = () => {
   return (
     <div className="min-h-screen bg-gray-50">
       {/* Header */}
-      <div className="bg-white shadow-sm border-b">
+      <div className="bg-red shadow-sm border-b ">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center py-4">
             <div className="flex items-center">
